85d29c47a2bdfbe95cc202ace6e0dc8bdc000be2
Danila Sergeevich <danya09kz@gmail.com>
2022-08-17 18:25:15 +0600

    initiate MUI lib

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SearchAppBar from "./components/SearchAppBar/SearchAppBar";
+import BasicSpeedDial from "./components/SpeedDial/SpeedDial";
+
+function App() {
+	return (
+		<>
+			<SearchAppBar/>
+			<BasicSpeedDial/>
+		</>
+	);
+}
+
+export default App;
